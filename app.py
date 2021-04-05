83efd901acab8bf297b37dba21429b2854f3763e
hzhang <hzhang@HRZ-1069.local>
2021-04-05 23:22:44 +0900

    add button, but not quite as expected

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 from random_choice import choose_a_word
 
 app = Flask(__name__)
 
 @app.route('/')
 def new_page():
-	return choose_a_word()
+	return render_template("index.html")#choose_a_word()
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
     
