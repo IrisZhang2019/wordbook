1dead6e669c4d71d6fb23ed7fbc12ff9d56cbf28
hzhang <hzhang@HRZ-1069.local>
2021-04-06 23:33:05 +0900

    try js, so much not yet learnt

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify, render_template, request
 from random_choice import choose_a_word
 
 app = Flask(__name__)
 
+'''
+@app.route('/_add_numbers')
+def add_numbers():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result=a + b)
+'''
+
 @app.route('/')
 def new_page():
-	return render_template("index.html")#choose_a_word()
+	return render_template("index.html", word=choose_a_word())
 
 
 if __name__ == '__main__':
    app.run(debug = True)
-    
+
+
